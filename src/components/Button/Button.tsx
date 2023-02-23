0057cb8875d2df015b76b122018af5c64d4bdcba
Connor Tyrrell <connor@brewgene.com>
2023-02-22 21:53:57 -0500

    Updated button to accept a Variant prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,35 @@
-import * as React from "react";
+import React, { useMemo } from "react";
 import {
   Button as PaperButton,
-  ButtonProps,
+  ButtonProps as PaperButtonProps,
   withTheme,
+  useTheme,
 } from "react-native-paper";
+import styles from "./ButtonStyles";
+
+interface ButtonProps extends PaperButtonProps {
+   variant?: 'primary' | 'primaryOnDark' | 'secondary' | 'secondaryOnDark';
+}
 
 const Button = (props: ButtonProps) => {
-  const { mode = "contained", ...restOfProps } = props;
-  return <PaperButton mode={mode} {...restOfProps} />;
+  const { mode = "contained", variant = 'primary', ...restOfProps } = props;
+  const theme = useTheme();
+  const ss = useMemo(() => styles(theme), [theme]);
+  
+  let buttonColor = theme.colors.primary;
+  let textColor = theme.colors.onPrimary;
+  if(variant === 'primaryOnDark') {
+    buttonColor = theme.colors.onPrimary;
+    textColor = theme.colors.primary;
+  } else if (variant === 'secondary') {
+    buttonColor = theme.colors.secondary;
+    textColor = theme.colors.onSecondary;
+  } else if (variant === 'secondaryOnDark') {
+    buttonColor = theme.colors.onSecondary;
+    textColor = theme.colors.secondary;
+  }
+
+  return <PaperButton mode={mode} buttonColor={buttonColor} textColor={textColor} {...restOfProps} />;
 };
 
 export default withTheme(Button);

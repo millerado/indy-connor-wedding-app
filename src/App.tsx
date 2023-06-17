051edb6f6556a2b6abedf9a8e83993148e2db389
Connor Tyrrell <connor@brewgene.com>
2023-06-17 18:36:05 -0400

    Possibly fix issue so badges update when getting notifications

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
   }),
 });
 

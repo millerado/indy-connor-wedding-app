b948f93c9451ba34dc061c0683c39defc538b372
Connor Tyrrell <connor@brewgene.com>
2023-07-08 15:42:31 -0400

    Faster reaction of marking notification as read

--- src/screens/NotificationsScreen/NotificationsScreen.tsx
+++ src/screens/NotificationsScreen/NotificationsScreen.tsx
@@ -25,10 +25,7 @@ import styles from "./NotificationsScreenStyles";
 const NotificationsScreen = ({ navigation, route }) => {
   const theme = useTheme();
   const ss = useMemo(() => styles(theme), [theme]);
-  const [loading, setLoading] = useState(false);
   const { allNotifications } = useContext(DataContext);
-
-  const { width } = calcDimensions();
 
   const navToLink = (targetType, id) => {
     // console.log('-- Notification Navigation --', targetType, id);
@@ -40,80 +37,7 @@ const NotificationsScreen = ({ navigation, route }) => {
   };
 
   const renderItem = useCallback(({ item }) => {
-    const linking = item.linking ? JSON.parse(item.linking) : null;
-    const iconName = linking.icon?.iconDetails || "alert";
-    let centerWidth = width - (typography.fontSizeL * 2 + 20);
-    if (!item.read) {
-      centerWidth = centerWidth - (typography.fontSizeL * 2 + 15);
-    }
-
-    const renderRightActions = () => {
-      return (
-        <View style={{backgroundColor: theme.colors.error, flex: 1, alignItems: 'center', justifyContent: 'flex-end', paddingRight: 10, flexDirection: 'row'}}>
-          <Text color={theme.colors.onPrimary}>
-            Delete Forever
-          </Text>
-          <Icon name="delete" size={typography.fontSizeL * 1.5} color={theme.colors.onPrimary} style={{paddingLeft: 10}} />
-        </View>
-      );
-    };
-
-    const onSwipeRight = () => {
-      // console.log('Swiped Right');
-      DataStore.delete(Notifications, item.id);
-    }
-
-    return (
-      <GestureHandlerRootView>
-        <Swipeable
-          renderRightActions={renderRightActions}
-          onSwipeableRightOpen={onSwipeRight}
-        >
-          <ConditionalWrapper
-            condition={linking.targetType}
-            wrapper={(children) => (
-              <Pressable onPress={() => navToLink(linking.targetType, linking.id)}>
-                {children}
-              </Pressable>
-            )}
-          >
-            <View style={{ padding: 10, flexDirection: "row", flex: 0, justifyContent: 'space-between', alignItems: 'center', backgroundColor: theme.colors.background }}>
-              <View style={{ paddingRight: 10, justifyContent: "center", width: (typography.fontSizeL * 2) + 10 }}>
-                <Icon name={iconName} size={typography.fontSizeL * 2} />
-              </View>
-              <View style={{ justifyContent: "center", width: centerWidth }}>
-                {item.subject !== 'Camp Conndigo' && (
-                  <Text size={TextSizes.L} bold>
-                    {item.subject}
-                  </Text>
-                )}
-                <FormatTextWithMentions
-                  text={item.messageBody}
-                  size={TextSizes.L}
-                  style={{ marginRight: 10 }}
-                />
-              </View>
-              {!item.read && (
-                <Pressable
-                  onPress={async () => {
-                    const originalItem = await DataStore.query(Notifications, item.id);
-                    DataStore.save(
-                      Notifications.copyOf(originalItem, (updated) => {
-                        updated.read = true;
-                      })
-                    );
-                  }}
-                >
-                  <View style={{ paddingLeft: 10, justifyContent: "center" }}>
-                    <Icon name={"new"} size={typography.fontSizeL * 2} />
-                  </View>
-                </Pressable>
-              )}
-            </View>
-          </ConditionalWrapper>
-        </Swipeable>
-      </GestureHandlerRootView>
-    );
+    return <RenderNotificationItem item={item} navToLink={navToLink} />;RenderNotificationItem
   }, []);
 
   const keyExtractor = useCallback((item) => item.id, []);
@@ -124,7 +48,7 @@ const NotificationsScreen = ({ navigation, route }) => {
 
   return (
     <View style={ss.pageWrapper}>
-      {loading || allNotifications?.length === 0 ? (
+      {allNotifications?.length === 0 ? (
         <View style={ss.pageActivityIndicatorWrapper}>
           <ActivityIndicator size={60} />
         </View>
@@ -147,3 +71,87 @@ const NotificationsScreen = ({ navigation, route }) => {
 };
 
 export default NotificationsScreen;
+
+const RenderNotificationItem = (props) => {
+  const { item, navToLink } = props;
+  const { width } = calcDimensions();
+  const theme = useTheme();
+  const ss = useMemo(() => styles(theme), [theme]);
+  const [read, setRead] = useState(item.read);
+
+  const linking = item.linking ? JSON.parse(item.linking) : null;
+  const iconName = linking.icon?.iconDetails || "alert";
+  let centerWidth = width - (typography.fontSizeL * 2 + 20);
+  if (!read) {
+    centerWidth = centerWidth - (typography.fontSizeL * 2 + 15);
+  }
+
+  const renderRightActions = () => {
+    return (
+      <View style={{backgroundColor: theme.colors.error, flex: 1, alignItems: 'center', justifyContent: 'flex-end', paddingRight: 10, flexDirection: 'row'}}>
+        <Text color={theme.colors.onPrimary}>
+          Delete Forever
+        </Text>
+        <Icon name="delete" size={typography.fontSizeL * 1.5} color={theme.colors.onPrimary} style={{paddingLeft: 10}} />
+      </View>
+    );
+  };
+
+  const onSwipeRight = () => {
+    // console.log('Swiped Right');
+    DataStore.delete(Notifications, item.id);
+  }
+
+  return (
+    <GestureHandlerRootView>
+      <Swipeable
+        renderRightActions={renderRightActions}
+        onSwipeableRightOpen={onSwipeRight}
+      >
+        <ConditionalWrapper
+          condition={linking.targetType}
+          wrapper={(children) => (
+            <Pressable onPress={() => navToLink(linking.targetType, linking.id)}>
+              {children}
+            </Pressable>
+          )}
+        >
+          <View style={{ padding: 10, flexDirection: "row", flex: 0, justifyContent: 'space-between', alignItems: 'center', backgroundColor: theme.colors.background }}>
+            <View style={{ paddingRight: 10, justifyContent: "center", width: (typography.fontSizeL * 2) + 10 }}>
+              <Icon name={iconName} size={typography.fontSizeL * 2} />
+            </View>
+            <View style={{ justifyContent: "center", width: centerWidth }}>
+              {item.subject !== 'Camp Conndigo' && (
+                <Text size={TextSizes.L} bold>
+                  {item.subject}
+                </Text>
+              )}
+              <FormatTextWithMentions
+                text={item.messageBody}
+                size={TextSizes.L}
+                style={{ marginRight: 10 }}
+              />
+            </View>
+            {!read && (
+              <Pressable
+                onPress={async () => {
+                  setRead(true);
+                  const originalItem = await DataStore.query(Notifications, item.id);
+                  DataStore.save(
+                    Notifications.copyOf(originalItem, (updated) => {
+                      updated.read = true;
+                    })
+                  );
+                }}
+              >
+                <View style={{ paddingLeft: 10, justifyContent: "center" }}>
+                  <Icon name={"new"} size={typography.fontSizeL * 2} />
+                </View>
+              </Pressable>
+            )}
+          </View>
+        </ConditionalWrapper>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+};

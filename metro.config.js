ee7726db9f623534df419e95f3df10257a29c9a2
Connor Tyrrell <connor@brewgene.com>
2025-07-19 16:54:45 -0400

    Got builds running, next is fixing Expo

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,22 @@
-const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// const config = getDefaultConfig(__dirname);
+
+// config.resolver.sourceExts.push("ios.js", "android.js");
+
+// module.exports = config;
+
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts.push("ios.js", "android.js");
+// config.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts = config.resolver.sourceExts || [];
+config.resolver.unstable_enablePackageExports = false;
+if (!config.resolver.sourceExts.includes("cjs")) {
+  config.resolver.sourceExts.push("cjs");
+}
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
